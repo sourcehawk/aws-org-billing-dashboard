9b3dc3b0b0f5dfd45905930d1867fdc05eff6231
sourcehawk <agir96@gmail.com>
2023-12-17 19:31:12 +0100

    update references (#9)
    * update references
    
    * update chart version

--- terraform/_providers.tf
+++ terraform/_providers.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1" # CUR is only available in us-east-1
   default_tags {
     tags = {
-      "project"     = "https://github.com/hauks96/aws-org-billing-dashboard"
+      "project"     = "https://github.com/sourcehawk/aws-org-billing-dashboard"
       "environment" = var.environment
     }
   }
